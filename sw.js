820bdf3d608a9f5836be4d40967b8c8dc5fe5b23
Himanshu Deshmukh <117311840+Himanshu4-Deshmukh@users.noreply.github.com>
2024-09-02 00:30:48 +0530

    Update and rename service-worker.js to sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+// sw.js
+self.addEventListener('install', event => {
+    // Perform install steps
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
